c0936381cc2a899a183305b5b4001908091500db
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2020-10-07 15:12:32 -0400

    add spuport email

--- src/screens/SupportScreen/SupportScreen.js
+++ src/screens/SupportScreen/SupportScreen.js
@@ -6,7 +6,13 @@ export default function SupportScreen() {
   return (
     <div className="support-container">
       <h3>Support</h3>
-      <h5>More coming soon!</h5>
+      <h5>
+        For support requests, please email&nbsp;
+        <a href="mailto:support@tastinggrounds.com">
+          support@tastinggrounds.com
+        </a>
+        .
+      </h5>
     </div>
   );
 }
