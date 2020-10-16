23dbe32d67632ce54b57ecbe9fd4e6d3fe85d1c7
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2020-10-16 15:33:31 -0400

    Updates

--- precache-manifest.18671638531ab4f324214e458eef24a1.js
+++ precache-manifest.18671638531ab4f324214e458eef24a1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "da6f13ad5a967629cebe0f76db44de95",
+    "revision": "7c77ed950584e5562d5b04c7f6d1e619",
     "url": "/index.html"
   },
   {
-    "revision": "6a874d40974c09a3af2d",
-    "url": "/static/css/main.aa0d28c9.chunk.css"
+    "revision": "3ea8973df90669bfb3bc",
+    "url": "/static/css/main.40007a47.chunk.css"
   },
   {
     "revision": "3a05f6dc3113d6605f7a",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.53fa5092.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a874d40974c09a3af2d",
-    "url": "/static/js/main.f5a55c27.chunk.js"
+    "revision": "3ea8973df90669bfb3bc",
+    "url": "/static/js/main.b1912dd9.chunk.js"
   },
   {
     "revision": "42350536d0e02bc6d269",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.183e88c5857e0f81326910902095cc74.js"
+  "/precache-manifest.18671638531ab4f324214e458eef24a1.js"
 );
 
 self.addEventListener('message', (event) => {
