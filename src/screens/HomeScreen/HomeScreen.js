1906291925de82a0b4f33d18b3428d130e81ffe4
Peter Nancarrow <plnancarrow@gmail.com>
2021-03-28 20:12:29 -0400

    address significant web vital issue

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -8,11 +8,14 @@ export default function HomeScreen() {
   return (
     <div className="HomeScreenContainer">
       <div className="HomeScreenContentContainer">
-        <img
-          className="ScreenshotImage"
-          src="assets/app-home-screenshot.png"
-          alt="app screenshot"
-        />
+        <picture>
+          <source srcSet="https://production.cdntg.xyz/assets/marketing-site/app-home-screenshot.webp" type="image/webp" />
+          <img
+            className="ScreenshotImage"
+            src="https://production.cdntg.xyz/assets/marketing-site/app-home-screenshot.png"
+            alt="app screenshot"
+          />
+        </picture>
         <div className="HomeScreenContentTextContainer">
           <Heading size={Heading.SIZES.XXLARGE}>Brew Coffee Together</Heading>
           <Heading size={Heading.SIZES.XSMALL}>A coffee community where you can log your brews, rate & review your coffees, discover new roasters, and share with your friends.</Heading>
