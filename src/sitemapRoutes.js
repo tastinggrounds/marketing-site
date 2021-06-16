9317145b3d224666d425b64903ad1d15c0e2ef24
Peter Nancarrow <plnancarrow@gmail.com>
2021-06-15 23:34:03 -0400

    add to sitemap

--- src/sitemapRoutes.js
+++ src/sitemapRoutes.js
@@ -7,6 +7,7 @@ export default (
     <Route path='/?/learn' />
     <Route path='/?/learn/information-standards' />
     <Route path='/?/learn/grind-sizes' />
+    <Route path='/?/learn/flavor-profiles' />
     <Route path='/?/learn/tasting-notes' />
     <Route path='/?/contact' />
     <Route path='/?/privacy' />
