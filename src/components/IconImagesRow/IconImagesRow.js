f5f2352ebe8f66195b4bb67644b64dcde9a40684
Peter Nancarrow <plnancarrow@gmail.com>
2021-11-27 00:35:34 -0500

    fix images & size

--- src/components/IconImagesRow/IconImagesRow.js
+++ src/components/IconImagesRow/IconImagesRow.js
@@ -7,17 +7,17 @@ export default function IconImagesRow() {
     <div className="IconsImagesContainer">
         <img 
             className="CoffeesCirlceImage" 
-            src={ process.env.PUBLIC_URL +"assets/coffee-circle.png" }
+            src={ process.env.PUBLIC_URL +"/assets/coffee-circle.png" }
             alt="tasting grounds coffee icon" 
         />
         <img 
             className="RoastersCirlceImage" 
-            src={ process.env.PUBLIC_URL + "assets/roaster-circle.png" } 
+            src={ process.env.PUBLIC_URL + "/assets/roaster-circle.png" } 
             alt="tasting grounds roaster icon" 
         />
         <img 
             className="UsersCirlceImage" 
-            src={process.env.PUBLIC_URL + "assets/user-circle.png" }
+            src={process.env.PUBLIC_URL + "/assets/user-circle.png" }
             alt="tasting grounds user icon" 
         />
     </div>
