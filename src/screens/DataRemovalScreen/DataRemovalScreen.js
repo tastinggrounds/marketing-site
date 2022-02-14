8244935e6b1b7a8d9b4d97d263a1ab2560d52406
Patrick Bacon-Blaber <pablaber225@gmail.com>
2022-02-14 15:14:10 -0500

    BREW-306: Add data removal page

---
+++ src/screens/DataRemovalScreen/DataRemovalScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './DataRemovalScreen.css';
+
+export default function DataRemovalScreen() {
+  return (
+    <div className="DataRemovalScreenContainer">
+      <p>
+        To request that your user data be removed, please email us at&nbsp;
+        <a href="mailto:support@tastinggrounds.com">support@tastinggrounds.com</a>
+        .
+      </p>
+    </div>
+  );
+}
