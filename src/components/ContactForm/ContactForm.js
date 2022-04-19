274a9a94114d9bdac97a78989d55abfca2ffbcba
Peter Nancarrow <plnancarrow@gmail.com>
2022-04-19 01:04:03 -0400

    sucess message

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -14,42 +14,55 @@ export default function ContactForm() {
     }
   }, []);
 
+  function onClick() {
+    setSuccess(false);
+    window.history.replaceState(null, '', '/contact');
+  }
+
   return (
     <div>
       {success && (
-        <p style={{ color: 'green' }}>
-          Thanks for your message!
-        </p>
+        <div className="successMessage">
+          Thanks for contacting Tasting Grounds! We will follow-up with you by email
+          as soon as possible.
+
+          Don&apos;t forget to checkout our feature roadmap or to submit your feature ideas on our
+          <a href="https://ideas.tastinggrounds.com"> ideas board!</a>
+          <p />
+          <button type="submit" onClick={onClick}>Back</button>
+        </div>
       )}
-      <form
-        name="contactForm"
-        action={actionUrl}
-        method="POST"
-        data-netlify="true"
-        netlify-honeypot="bot-field"
-        className="contactForm"
-      >
-        <input
-          type="hidden"
-          name="form-name"
-          value="contactForm"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Your email"
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Your name"
-        />
-        <textarea
-          name="message"
-          placeholder="Messaage"
-        />
-        <button type="submit">Submit</button>
-      </form>
+      {!success && (
+        <form
+          name="contactForm"
+          action={actionUrl}
+          method="POST"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          className="contactForm"
+        >
+          <input
+            type="hidden"
+            name="form-name"
+            value="contactForm"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+          />
+          <textarea
+            name="message"
+            placeholder="Messaage"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 }
